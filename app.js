f3134d03f44356406d47ee006ae69eb8dcefa37c
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2025-11-09 22:31:48 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// ==========================================
+//         Les types d'erreurs
+// ==========================================
+
+// ========== Les erreurs natives ===========
+
+// 1. Type d'erreur : SyntaxError (Erreur de syntaxe dans le code)
+
+
+//  2. Type d'erreur : ReferenceError (ReferenceError)
+
+
+// 3. Type d'erreur : TypeError (Type inattendu null ou undefined)
+
+
+// 4. Type d'erreur : RangeError (Valeur hors limites)
+
+
+// ========== Les erreurs contextuelles ===========
+
+// 1. Erreurs liées au DOM (Problèmes lors de la manipulation des éléments HTML)
+
+
+// 2. Erreurs d'asynchronisme (Problèmes avec les promesses et async/await)
+
+
+// 3. Erreurs d'environnement (Différences entre navigateur et Node.js)
+
+
+
+// ==========================================
+//         Les outils de debugging
+// ==========================================
+
+// 1. console.log()
+
+
+// 2. console.error()
+
+
+// 3. console.table()
+
+
+// 4. console.trace()
+
+
+// 5. console.warn()
+
+
+// 6. console.info()
+
+
+// 7. console.assert(c,m)
+
+
+// 8. console.clear()
+
+
+// ==========================================
+//                  DevTools
+// ==========================================
+
+// example with validation et try catch.
